1d0f7666568d531d118a9003ca0d47ea912bc662
VZelinskiy <voldimar93@mail.ru>
2018-11-07 07:07:56 +0200

    add accountActivity

---
+++ SmartPass/app/src/main/java/com/example/vladimir/smartpass/AccountActivity.java
@@ -0,0 +1,12 @@
+package com.example.vladimir.smartpass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- SmartPass/app/src/main/java/com/example/vladimir/smartpass/MainActivity.java
+++ SmartPass/app/src/main/java/com/example/vladimir/smartpass/MainActivity.java
@@ -1,18 +1,33 @@
 package com.example.vladimir.smartpass;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView accountList;
+    private Button addAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView accountList = (RecyclerView) findViewById(R.id.accountList);
+        findUIComponents();
 
+    }
+
+    public void findUIComponents(){
+        accountList = findViewById(R.id.accountList);
+        addAccount = findViewById(R.id.addAccount);
+    }
+
+    public void addButton(View view){
+        Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+        startActivity(intent);
     }
 }
