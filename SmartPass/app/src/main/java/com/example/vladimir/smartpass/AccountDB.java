d724e15c612d6b73d285dfc1ad9cec7406319883
VZelinskiy <voldimar93@mail.ru>
2018-11-07 12:16:38 +0200

    refactoring

--- SmartPass/app/src/main/java/com/example/vladimir/smartpass/AccountDB.java
+++ SmartPass/app/src/main/java/com/example/vladimir/smartpass/AccountDB.java
@@ -1,13 +1,13 @@
 package com.example.vladimir.smartpass;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import java.sql.SQLTransactionRollbackException;
-
-public class SmartPassDBHelper extends SQLiteOpenHelper {
+public class AccountDB extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "PassBase";
     private static final int DATABASE_VERSION = 1;
 
@@ -18,14 +18,17 @@ public class SmartPassDBHelper extends SQLiteOpenHelper {
     public static final String LOGIN = "login";
     public static final String PASS = "pass";
 
+    public SQLiteDatabase database;
+
     public static final String DATABASE_CREATE = "create table " + TABLE_NAME
             + " (_id integer primary key autoincrement, "
             + SITE_ADDRESS + " text, " + SITE_NAME + " text not null, "
             + DESCRIPTION + " text, " + LOGIN + " text, "
-            + PASS + " text, ";
+            + PASS + " text)";
 
-    public SmartPassDBHelper(Context context){
+    public AccountDB(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        database = getWritableDatabase();
     }
 
     @Override
@@ -35,9 +38,28 @@ public class SmartPassDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.w(SmartPassDBHelper.class.getName(), "Upgrading database from version " + oldVersion + " to "
+        Log.w(AccountDB.class.getName(), "Upgrading database from version " + oldVersion + " to "
         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public long createRecords(String siteAddress, String siteName, String description, String login, String pass){
+        ContentValues values = new ContentValues();
+        values.put(SITE_ADDRESS, siteAddress);
+        values.put(SITE_NAME, siteName);
+        values.put(DESCRIPTION, description);
+        values.put(LOGIN, login);
+        values.put(PASS, pass);
+        return database.insert(TABLE_NAME, null, values);
+    }
+
+    public Cursor selectSiteNames(){
+        String[] cols = new String[] {SITE_NAME};
+        Cursor cursor = database.query(true, TABLE_NAME, cols, null, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
     }
 }
--- SmartPass/app/src/main/java/com/example/vladimir/smartpass/MainActivity.java
+++ SmartPass/app/src/main/java/com/example/vladimir/smartpass/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.vladimir.smartpass;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView accountList;
+    private ListView accountList;
     private Button addAccount;
+    private AccountDB accountDB;
+    private Cursor cursor;
+    private SimpleCursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findUIComponents();
+        loadAccountsList();
 
+    }
+
+    public void loadAccountsList() {
+        accountDB = new AccountDB(this);
+        cursor = accountDB.selectSiteNames();
+        startManagingCursor(cursor);
+        String[] from = new String[]{accountDB.SITE_NAME};
+        int[] to = new int[] {R.id.accountList};
+        cursorAdapter = new SimpleCursorAdapter(this, R.layout.account, cursor, from, to);
+        accountList.setAdapter(cursorAdapter);
     }
 
     public void findUIComponents(){
