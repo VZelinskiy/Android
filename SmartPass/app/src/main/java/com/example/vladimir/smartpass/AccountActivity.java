ada2d235f4a8dd630aaaf4e7f0bd57547398c17a
VZelinskiy <voldimar93@mail.ru>
2018-11-07 08:51:33 +0200

    AddButton OnClick

--- SmartPass/app/src/main/java/com/example/vladimir/smartpass/AccountActivity.java
+++ SmartPass/app/src/main/java/com/example/vladimir/smartpass/AccountActivity.java
@@ -8,5 +8,6 @@ public class AccountActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.account);
     }
 }
