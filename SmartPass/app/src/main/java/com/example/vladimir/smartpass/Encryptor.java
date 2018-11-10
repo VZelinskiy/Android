90f43a053ca68a694404017467b72076df5bac0c
VZelinskiy <voldimar93@mail.ru>
2018-11-10 19:54:17 +0200

    declare Encryptor

---
+++ SmartPass/app/src/main/java/com/example/vladimir/smartpass/Encryptor.java
@@ -0,0 +1,20 @@
+package com.example.vladimir.smartpass;
+
+public class Encryptor {
+    private String inputString;
+    private String result;
+    private static final String key = "justmonika";
+
+    public Encryptor(String inputString) {
+        this.inputString = inputString;
+    }
+    
+    public String Encrypt(){
+        return result;
+    }
+
+    public String Decrypt(){
+        return result;
+    }
+
+}
